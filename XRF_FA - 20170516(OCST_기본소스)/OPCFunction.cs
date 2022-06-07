4c154d1f30ea064be3a3ae6553ec54e7aa1fc4f9
Nguyen Thanh Dat <37483861+thanhdat6716@users.noreply.github.com>
2022-06-07 14:42:12 +0900

    Upload Original Project
    This is project before modify

---
+++ XRF_FA - 20170516(OCST_기본소스)/OPCFunction.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading;
+
+using opcNet.IF.SEM;
+namespace XRF_FA
+{
+    public class OPCFunction: MainForm
+    {
+        //// Org(자동화 테스트전(2016-11-01))
+        //public const int TAG_GROUP01 = 25;
+        //public const int TAG_CONV_LOC = 12;
+        //public const int TAG_BUFFER = 80;
+        //public const int TAG_SIGNAL = 67;
+
+        //// 2016-11-01 변경(자동화테스트)
+        //public const int TAG_GROUP01 = 28;
+        //public const int TAG_CONV_LOC = 20;
+        //public const int TAG_BUFFER = 132;//80;
+        //public const int TAG_SIGNAL = 67;
+
+        // 2016-11-12 변경(BRUKER)
+        public const int TAG_GROUP01 = 28;
+        public const int TAG_CONV_LOC = 20;
+        public const int TAG_BUFFER = 132;//80;
+        public const int TAG_SIGNAL = 67;
+        public const int TAG_GROUP02 = 15;
+
+
+        //// Add
+        //public const int TAG_GROUP01 = 32;
+        //public const int TAG_CONV_LOC = 13;
+        //public const int TAG_BUFFER = 132;
+        //public const int TAG_SIGNAL = 67;
+        //public const int TAG_ALARM = 46;
+
+        ////// Mod2
+        //public const int TAG_GROUP01 = 27;
+        //public const int TAG_CONV_LOC = 13;
+        //public const int TAG_BUFFER = 132;
+        //public const int TAG_SIGNAL = 67;
+        ////public const int TAG_ALARM = 46;
+
+        //public string[] Group_Names = new string[3];
+        //public string[] TagName_Group01 = new string[TAG_GROUP01];
+        //public string[] TagName_Conv_Loc = new string[TAG_CONV_LOC];
+        //public string[] TagName_Buffer = new string[TAG_BUFFER];
+
+        // 2016-11-12 변경(BRUKER)
+        public string[] Group_Names = new string[4];
+        public string[] TagName_Group01 = new string[TAG_GROUP01];
+        public string[] TagName_Conv_Loc = new string[TAG_CONV_LOC];
+        public string[] TagName_Buffer = new string[TAG_BUFFER];
+        public string[] TagName_Group02 = new string[TAG_GROUP02];
+
+        #region  // OPC 공통 Method
+
+        public void init_TagName()
+        {
+            for (int i = 0; i < TAG_BUFFER; i++)
+            {
+                TagName_Buffer[i] = "Buffer" + (i + 1).ToString();
+            }
+
+
+        }
+
+        /// <summary>
+        /// 해당 그룹에 소속된 모든 Tag들의 값을 비동기로 Write 한다.
+        /// </summary>
+        /// <param name="m_opcMgr"></param>
+        /// <param name="sGroupName"></param>
+        /// <param name="iGroupTagCount"></param>
+        /// <param name="oWriteDatas"></param>
+        /// <returns></returns>
+        public static int _OPC_Write_Ascyn_Group_Tags(opcMgrClass m_opcMgr, string sGroupName, ref object[] oWriteDatas)
+        {
+            if (m_opcMgr == null)   // OPC Server가 실행되지 않았다.
+            {
+                return 999;
+            }
+
+            int iTagCount = oWriteDatas.Length;
+
+            int iResFunc = m_opcMgr.opcWriteAsyncGroupTags(sGroupName, iTagCount, ref oWriteDatas, 3);
+            Thread.Sleep(300);
+            return iResFunc;
+        }
+
+        /// <summary>
+        /// 해당 그룹에 소속된 일부 Tag들의 값을 비동기로 Write 한다.
+        /// </summary>
+        /// <param name="m_opcMgr"></param>
+        /// <param name="sGroupName"></param>
+        /// <param name="iGroupTagCount"></param>
+        /// <param name="iTagHandle"></param>
+        /// <param name="oWriteData"></param>
+        /// <returns></returns>
+        public static int _OPC_Writes_Ascyn_Tags(opcMgrClass m_opcMgr, string sGroupName, ref int[] iTagHandle, ref object[] oWriteData)
+        {
+            if (m_opcMgr == null)   // OPC Server가 실행되지 않았다.
+            {
+                return 999;
+            }
+
+            int iTagCount = iTagHandle.Length;
+
+            int iResFunc = m_opcMgr.opcWritesAsync(sGroupName, iTagCount, ref iTagHandle, ref oWriteData, 5);
+
+            Thread.Sleep(300);
+
+            return iResFunc;
+        }
+
+        /// <summary>
+        ///  해당 그룹에 소속된 모든 Tag들의 값을 읽는다.
+        /// </summary>
+        /// <param name="m_opcMgr"></param>
+        /// <param name="sGroupName"></param>
+        /// <param name="iTagCount"></param>
+        /// <returns></returns>
+        public static int _OPC_Read_Group_Tags(opcMgrClass m_opcMgr, string sGroupName, int iTagCount, ref object[] oReadVals, ref int[] iQualities)
+        {
+            if (m_opcMgr == null)   // OPC Server가 실행되지 않았다.
+            {
+                return 999;
+            }
+
+            int iResFunc = m_opcMgr.opcReadGroupTags(sGroupName, iTagCount, ref oReadVals, ref iQualities);
+
+            Thread.Sleep(300);
+
+            return iResFunc;
+
+        }
+
+        #endregion
+
+    }
+}
---
+++ XRF_FA - 20170516(OCST_기본소스)/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace XRF_FA
+{
+    static class Program
+    {
+        /// <summary>
+        /// 해당 응용 프로그램의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            bool flag = false;
+            int ProgId = Process.GetCurrentProcess().Id;
+            Process[] p = Process.GetProcessesByName("XRF_FA");
+
+            if (p.Length > 1)
+            {
+                for (int i = 0; i < p.Length; i++)
+                {
+                    if (p[i].Id != ProgId)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        flag = true;
+                    }
+                }
+
+            }
+
+            if (flag)
+            {
+                MessageBox.Show("프로그램이 이미 실행되고 있습니다.", "경고");
+            }
+            else
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm());
+            }
+        }
+    }
+}
